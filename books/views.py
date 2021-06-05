0930ed63edeab4fb6467cf6699b83dafe9ff20ff
Arun Misra <arunmisra2273@gmail.com>
2021-06-05 20:54:08 +0530

    Load data from json file

--- books/views.py
+++ books/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
+import json
+
+booksData = open('D:/pydjtwo/bookstore-project/books.json').read()
+data = json.loads(booksData)
+
 # Create your views here.
 
 def index(request):
-    context = {'name': 'Purnima'}
+    context = {'books': data}
     return render(request, 'books/index.html', context)
