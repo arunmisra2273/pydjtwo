ee84822037834b7b4a6847dff04ffe10727a3dfa
Arun Misra <arunmisra2273@gmail.com>
2021-06-05 20:24:47 +0530

    First HTML view and context

--- books/views.py
+++ books/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello World')
+    context = {'name': 'Purnima'}
+    return render(request, 'books/index.html', context)
