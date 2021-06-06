1ef282135d811ff1591cf8a999ccab5c552eb6a5
Arun Misra <arunmisra2273@gmail.com>
2021-06-06 15:17:31 +0530

    Dynamic Routing

--- books/views.py
+++ books/views.py
@@ -10,3 +10,11 @@ data = json.loads(booksData)
 def index(request):
     context = {'books': data}
     return render(request, 'books/index.html', context)
+
+def show(request, id):
+    singleBook = []
+    for book in data:
+        if book['id'] == id:
+            singleBook = book
+    context = {'book': singleBook}
+    return render(request, 'books/show.html', context)
