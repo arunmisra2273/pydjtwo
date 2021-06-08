356a7b36379bd7bec6f9c11237e2a94172529e6e
Arun Misra <arunmisra2273@gmail.com>
2021-06-08 17:23:54 +0530

    Review Migration, Store and Display Review

--- books/urls.py
+++ books/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name="books.index"),
     path("<int:id>", views.show, name="books.show"),
+    path('review', views.review, name="books.review"),
 ]
--- books/views.py
+++ books/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from books.models import Book
+from django.shortcuts import render, get_object_or_404, redirect
+from books.models import Book, Review
 
 # Create your views here.
 
@@ -10,5 +10,12 @@ def index(request):
 
 def show(request, id):
     singleBook = get_object_or_404(Book, pk=id)
-    context = {'book': singleBook}
+    reviews = Review.objects.all()
+    context = {'book': singleBook, 'reviews': reviews}
     return render(request, 'books/show.html', context)
+
+def review(request):
+    body = request.POST['review']
+    newReview = Review(body=body)
+    newReview.save()
+    return redirect('/books')
