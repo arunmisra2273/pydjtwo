c10d44b9c77d804e36139c833d897b551d5a1de8
Arun Misra <arunmisra2273@gmail.com>
2021-06-08 15:46:14 +0530

    Populate HTML from DB

--- books/views.py
+++ books/views.py
@@ -1,20 +1,15 @@
 from django.shortcuts import render
-
-import json
-
-booksData = open('D:/pydjtwo/bookstore-project/books.json').read()
-data = json.loads(booksData)
+from books.models import Book
 
 # Create your views here.
 
 def index(request):
-    context = {'books': data}
+    dbData = Book.objects.all()
+    context = {'books': dbData}
     return render(request, 'books/index.html', context)
 
 def show(request, id):
-    singleBook = []
-    for book in data:
-        if book['id'] == id:
-            singleBook = book
+
+    singleBook = Book.objects.get(pk=id)
     context = {'book': singleBook}
     return render(request, 'books/show.html', context)
